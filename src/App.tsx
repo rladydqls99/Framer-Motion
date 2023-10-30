3f61f2ed4cb1a3803a43c939a892005166cfd7b2
rladydqls99 <rladydqls99@naver.com>
2023-10-31 00:09:36 +0900

    사각형에 점 네 개 찍기

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,18 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  place-self: center;
+  width: 70px;
+  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -25,10 +35,45 @@ const myVars = {
     transition: { type: "spring", bounce: 100, delay: 1 },
   },
 };
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      // 자식 컴포넌트에 순차적으로 딜레이를 줌
+      staggerChildren: 0.2,
+    },
+  },
+};
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    transition: {},
+    y: 0,
+  },
+};
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      {/* 자식 요소에 props가 자동으로 상속됨 */}
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }

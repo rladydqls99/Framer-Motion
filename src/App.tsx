1dd195cabf2b104dbfb27e3e5b749780025309e7
rladydqls99 <rladydqls99@naver.com>
2023-10-31 00:50:47 +0900

    화면 위치에 따라서 스케일 조정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,16 +9,6 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -30,33 +19,12 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 1, rotateZ: 90 },
-  click: { borderRadius: "100px", scale: 1 },
-  drag: { backgroundColor: "rgb(46,24, 123)", transition: { duration: 10 } },
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   return (
     <Wrapper>
-      {/* biggerBoxRef를 참조해서 Box의 움직임 제한 */}
-      <BiggerBox ref={biggerBoxRef}>
-        {/* 자식 요소에 props가 자동으로 상속됨 */}
-        <Box
-          drag
-          // 요소를 원위치로 돌리기
-          dragSnapToOrigin
-          // 요소를 벗어나서 당기는 탄력
-          dragElastic={0.5}
-          // 드래그 제약(움직이는 거리 제한 및 원위치 시킴)
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileDrag="drag"
-          whileHover="hover"
-          whileTap="click"
-        ></Box>
-      </BiggerBox>
+      <Box style={{ x: x, scale: scale }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
